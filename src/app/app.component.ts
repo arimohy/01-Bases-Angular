af9a6609278432ef0e139fbee2093d84f8d36013
Yhomira Yupayccana Lopa <yhomira.yupayccana@sofka.com.co>
2022-02-07 11:07:00 -0500

    metodo acumular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   titulo:String = 'Contador App 3';
   numero:number=10;
 
-  sumar(){
-    this.numero+=1;
-  }
-  restar(){
-    this.numero-=1;
+
+  acumular(valor:number){
+    this.numero+=valor;
   }
 }
