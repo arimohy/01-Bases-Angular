6003f39fea111440d5c820fb7ff96f48ee26c545
Yhomira Yupayccana Lopa <yhomira.yupayccana@sofka.com.co>
2022-02-09 17:22:05 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje{
+  nombre:string;
+  poder:number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // agregar(event: any){
+  //   event.preventDefault();
+  // }
+  nuevo:Personaje={
+    nombre: 'truck',
+    poder: 14000
+  }
+  agregar(){
+    console.log(this.nuevo)
+  }
+  cambiarNombre(event:any){
+    console.log(event.value)
   }
 
 }
